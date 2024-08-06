c3ec3c7c488f08fa56302bd822fde6c34a0cc5ee
Shashikant Badgujar <shashikant.badgujar@parallel-minds.com>
2024-08-06 14:54:55 +0530

    animals is project related to basic react concepts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+import './App.css'
+
+
 
 function App() {
+
+  const returnRandomAnimal = () => {
+    const animalArray = ["cat", "cow", "dog", "horse", "bird", "gator"];
+    let animalAtIndex = Math.round(Math.random() * (animalArray.length - 1))
+    console.log(animalArray[animalAtIndex]);
+    return animalArray[animalAtIndex];
+
+  }
+
+  const [animal, setAnimal] = useState([])
+  const handleClick = () => {
+    setAnimal([...animal, returnRandomAnimal()])
+  }
+
+  const renderedAnimal = animal.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button className="button-container" onClick={handleClick}>Add animals</button>
+      <div className="animal-list" >{renderedAnimal} </div>
     </div>
   );
+
+
+  /* 
+  
+    //array destructuring
+    function makeArray() {
+      return [14, 25, 33, 49];
+    }
+  
+    const [one, two] = makeArray()
+    console.log(one, two)
+  
+    const [count, setCount] = useState(0);
+    // console.log(useState(1));
+  
+    const handleClick = () => {
+      console.log("Animal clicled");
+      setCount(count + 1);
+      //setCount("hello")
+    } */
+
+
 }
 
 export default App;
